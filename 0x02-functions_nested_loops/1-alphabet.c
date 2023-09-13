0c94a2b6f389e96adc191783c22a2dac18ab73af
fuadmoin <fuadamoin@gmail.com>
2023-09-13 16:17:59 +0300

    Modify the answers.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always 0.
+ */
+
+print_alphabet(void)
+{
+char starter = 'a';
+while (starter <= 'z')
+{
+_putchar(starter);
+starter++;
+}
+_putchar('\n');
+}
