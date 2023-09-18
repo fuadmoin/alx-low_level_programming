775735bdd6931d658b3c0aebc090b45a6344a8d2
fuadmoin <fuadamoin@gmail.com>
2023-09-19 00:45:15 +0300

    Answer question 7.

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,7 +17,7 @@ if (len % 2 == 1)
 middle = ((len - 1) / 2) + 1;
 else
 middle = len / 2;
-for (i = len / 2; str[i] != '\0'; i++)
+for (i = middle; str[i] != '\0'; i++)
 _putchar(str[i]);
 _putchar('\n');
 }
