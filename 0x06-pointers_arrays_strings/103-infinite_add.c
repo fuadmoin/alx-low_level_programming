c0494c0f50b05aaa8b8ee6c0784f7f5d04e8ef6c
fuadmoin <fuadamoin@gmail.com>
2023-09-21 22:22:34 +0300

    Modify the answers.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,30 @@
 #include "main.h"
 #include <stdio.h>
+
+/**
+* rev_string - returns the string in reverse.
+* @s: pointer to a char
+* Return: nothing
+*/
+
+void rev_string(char *s)
+{
+int len = 0, i = 0;
+char *start = s;
+while (*s != '\0')
+{
+len++;
+s++;
+}
+
+for (i = 0; i < len / 2; i++)
+{
+char temp = start[i];
+start[i] = start[len - 1 - i];
+start[len - 1 - i] = temp;
+}
+}
+
 /**
  * infinite_add - adds two integers.
  * @n1: The first pointer
