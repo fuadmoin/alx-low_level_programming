d87b47e1286c48e4b919d7ab54c20be37a33c7df
fuadmoin <fuadamoin@gmail.com>
2023-10-02 10:00:07 -0700

    Modify the answer.

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+* _putchar - prints a character
+* @c: variable to be checked
+* Return: 0
+*/
+
+int _putchar(char c)
+{
+return (putchar(c));
+}
+
