b6ac7ed75af553aaefed4623e3f3676f460561b5
fuadmoin <fuadamoin@gmail.com>
2023-10-03 16:40:54 +0300

    Modify the answers.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -12,7 +12,7 @@ int count_words(char *str)
 int i, count = 0;
 
 for (i = 0; str[i]; i++)
-if ((str[i] == ' ' || str[i + 1] == '\0') && str[i - 1] != ' ')
+if ((str[i] == ' ' || str[i + 1] == '\0') && str[i] != ' ')
 count++;
 
 return (count);
