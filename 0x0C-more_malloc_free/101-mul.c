dc5e881540331bbdd900b02c1e045a8a762e239c
fuadmoin <fuadamoin@gmail.com>
2023-10-06 23:10:54 +0300

    Modify the answers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,33 +1,8 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-/**
- * _atoi - convert a string into an integer.
- *
- * @s: the string to use.
- *
- * Return: integer.
- */
-
-int _atoi(char *s)
-{
-int sign = 1, i = 0;
-unsigned int res = 0;
-
-while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
-{
-if (s[i] == '-')
-sign *= -1;
-i++;
-}
-while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
-{
-res = (res * 10) + (s[i] - '0');
-i++;
-}
-res *= sign;
-return (res);
-}
-
+#define ERR_MSG "Error"
 
 /**
  * _isdigit - checks if a string only contains digits
@@ -37,40 +12,90 @@ return (res);
  */
 int _isdigit(char *str)
 {
-int i;
+int index = 0;
 
-for (i = 0; str[i]; i++)
+while (str[index])
 {
-if (str[i] < '0' || str[i] > '9')
+if (str[index] < '0' || str[index] > '9')
 return (0);
+index++;
 }
-
 return (1);
 }
 
 /**
- * main - multiplies two positive numbers
- * @argc: number of arguments passed to the program
- * @argv: array of arguments passed to the program
- *
- * Return: 0 if successful, 1 if program fails
- */
-
-int main(int argc, char *argv[])
+* _strlen - returns the length of a string
+* @str: string to evaluate
+*
+* Return: the length of the string
+*/
+int _strlen(char *str)
 {
-long num1, num2, product;
+int index = 0;
 
-if (argc != 3 || !_isdigit(argv[1]) || !_isdigit(argv[2]))
+while (str[index] != '\0')
+{
+index++;
+}
+return (index);
+}
+
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
 {
 printf("Error\n");
 exit(98);
 }
 
-num1 = _atoi(argv[1]);
-num2 = _atoi(argv[2]);
-product = num1 *num2;
-
-printf("%ld\n", product);
-
+/**
+* main - multiplies two positive numbers
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+char *num1, *num2;
+int len_num1, len_num2, total_len, index, carry_over;
+int digit_num1, digit_num2, *result_array, flag = 0;
+num1 = argv[1], num2 = argv[2];
+if (argc != 3 || !_isdigit(num1) || !_isdigit(num2))
+errors();
+len_num1 = _strlen(num1);
+len_num2 = _strlen(num2);
+total_len = len_num1 + len_num2 + 1;
+result_array = malloc(sizeof(int) * total_len);
+if (!result_array)
+return (1);
+for (index = 0; index <= len_num1 + len_num2; index++)
+result_array[index] = 0;
+for (len_num1 = len_num1 - 1; len_num1 >= 0; len_num1--)
+{
+digit_num1 = num1[len_num1] - '0';
+carry_over = 0;
+for (len_num2 = _strlen(num2) - 1; len_num2 >= 0; len_num2--)
+{
+digit_num2 = num2[len_num2] - '0';
+carry_over += result_array[len_num1 + len_num2 + 1] + (digit_num1 *digit_num2);
+result_array[len_num1 + len_num2 + 1] = carry_over % 10;
+carry_over /= 10;
+}
+if (carry_over > 0)
+result_array[len_num1 + len_num2 + 1] += carry_over;
+}
+for (index = 0; index < total_len - 1; index++)
+{
+if (result_array[index])
+flag = 1;
+if (flag)
+_putchar(result_array[index] + '0');
+}
+if (!flag)
+_putchar('0');
+_putchar('\n');
+free(result_array);
 return (0);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -12,8 +12,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int _strlen(char *s);
+int _strlen(char *str);
 int _isdigit(char *str);
 int _atoi(char *s);
+void void errors(void);
 
 #endif
