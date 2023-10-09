3429ca71ecc75051ee1ba0c3170b982558a1cb58
fuadmoin <fuadamoin@gmail.com>
2023-10-10 01:13:56 +0300

    Answer structures typedef.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+
 typedef struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+char *name;
+float age;
+char *owner;
 } dog;
 
 typedef struct dog dog_t;
+
+
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
