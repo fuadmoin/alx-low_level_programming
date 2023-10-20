e7512f47c8a7df3f2a4b3604b8a97c7dc92ab768
fuadmoin <fuadamoin@gmail.com>
2023-10-20 22:41:56 +0300

    Modify the answers.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,19 @@
 section .data
     hello db 'Hello, Holberton', 0
+    format db "%s", 10, 0
 
 section .text
     global main
     extern printf
 
 main:
-    sub rsp, 8                         ; Align stack
-    mov rdi, format                    ; Format string
-    mov rsi, hello                     ; String to print
-    xor eax, eax                       ; Clear EAX register
-    call printf                        ; Call printf function
-    add rsp, 8                         ; Restore stack
-    xor eax, eax                       ; Return 0
+    ; Prepare arguments for printf
+    mov rdi, hello  ; format
+    xor rax, rax    ; vararg count
+
+    ; Call printf
+    call printf
+
+    ; Return from main
+    xor eax, eax
     ret
