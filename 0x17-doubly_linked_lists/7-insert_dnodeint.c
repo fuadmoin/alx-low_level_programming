611d1b192740e89695d2c2b56f47718646591786
fuadmoin <fuadamoin@gmail.com>
2023-12-08 05:28:14 +0300

    Answer doubly linked lists.

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,75 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: double pointer to the beginning of the linked list
+ * @idx: index at which to insert the new node
+ * @n: data to enter into new node
+ *
+ * Return: pointer to the new node, or NULL on failure
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+dlistint_t *head = *h, *new_node = NULL;
+unsigned int i = 1;
+
+if (idx == 0)
+new_node = add_dnodeint(h, n);
+else
+{
+if (head != NULL)
+while (head->prev != NULL)
+head = head->prev;
+while (head != NULL)
+{
+if (i == idx)
+{
+if (head->next == NULL)
+new_node = add_dnodeint_end(h, n);
+else
+{
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+return (NULL);
+new_node->n = n;
+new_node->next = head->next;
+new_node->prev = head;
+head->next->prev = new_node;
+head->next = new_node;
+}
+break;
+}
+head = head->next;
+i++;
+}
+
+}
+return (new_node);
+#include "lists.h"
+
+/**
+* sum_dlistint - sums all of the data of a dlistint_t linked list
+* @head: pointer to the beginning of the linked list
+*
+* Return: sum of all data, or 0 if the list is empty
+*/
+
+int sum_dlistint(dlistint_t *head)
+{
+dlistint_t *h = head;
+int sum = 0;
+
+if (head == NULL)
+{
+return (sum);
+}
+
+while (h != NULL)
+{
+sum += h->n;
+h = h->next;
+}
+return (sum);
+}
+}
