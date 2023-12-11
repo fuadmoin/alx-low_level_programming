b7b7834e71080a23db427ce07f49392b58206892
fuadmoin <fuadamoin@gmail.com>
2023-12-11 12:37:54 -0800

    Modify the answers.

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-wget -q -O myrand.so https://github.com/fuadmoin/alx-low_level_programming/0x18-dynamic_libraries/myrand.so
-export LD_PRELOAD=myrand.so
+export LD_PRELOAD=$PWD/myrand.so
